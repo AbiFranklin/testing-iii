e534e6118815c6e8383a7bd7f6ac9e148334c280
Abigayle Franklin <abifanklin@gmail.com>
2019-02-05 22:06:14 -0600

    MVP met

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,5 @@
-import Display from '../display/Display';
 import React from 'react';
-import { render, cleanup, fireEvent, getByTestId } from 'react-testing-library';
+import { render, cleanup, fireEvent, getByTestId, getByText } from 'react-testing-library';
 import Controls from './Controls';
 import 'jest-dom/extend-expect';
 
@@ -8,27 +7,21 @@ import 'jest-dom/extend-expect';
 afterEach(cleanup);
 
 describe('Controls Component', () => {
+    afterEach(cleanup);
+
     it('buttons that toggle state', () => {
         const component = render(<Controls />);
         component.getByTestId(/closebutton/i);
         component.getByTestId(/lockbutton/i);
     });
 
-    it('botton displays "close gate" if currently open', () => {
+
+    it('button displays "close gate" if currently open', () => {
         const component = render(<Controls />);
-        const button = component.getByTestId(/closebutton/i);
-        component.getByText('Close Gate');
+        component.getByText(/close gate/i);
     });
 
-    it('botton displays "close gate" if currently open', () => {
-        const component = render(<Controls />);
-        const button = component.getByTestId(/closebutton/i);
-        fireEvent.click(button);
-        component.getByText('Open Gate');
-    });
+
+
 });
 
-// Test away!
-// - buttons' text changes to reflect the state the door will be in if clicked
-// - the closed toggle button is disabled if the gate is closed
-// - the locked toggle button is disabled if the gate is open
